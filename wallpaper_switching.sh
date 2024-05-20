b6d27f1efe0f56933e97742074e36e0f8cda29f5
jakubhalik <jakubhalik1@gmail.com>
2024-05-21 00:52:30 +0200

    xinitrc

---
+++ wallpaper_switching.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+WALLPAPER_DIR="$HOME/do/w"
+WALLPAPERS=($(
+	find "$WALLPAPER_DIR" -type f \( -name '*.jpg' -o -name '*.png' \)
+))
+NUM_WALLPAPERS=${#WALLPAPERS[@]}
+
+while true; do
+	RANDOM_INDEX=$((RANDOM % NUM_WALLPAPERS))
+	SELECTED_WALLPAPER="${WALLPAPERS[$RANDOM_INDEX]}"
+	xwallpaper --zoom "$SELECTED_WALLPAPER"
+	wal -i "$SELECTED_WALLPAPER"
+	RANDOM_INTERVAL=$((RANDOM % 91 + 90))
+	sleep $RANDOM_INTERVAL
+done
+
