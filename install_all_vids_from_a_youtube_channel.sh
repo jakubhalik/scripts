25736a3c6f0995fc397248cde98baa1c534a4816
jakubhalik <jakubhalik1@gmail.com>
2024-05-29 13:35:13 +0200

    download even auto generated subs

--- install_all_vids_from_a_youtube_channel.sh
+++ install_all_vids_from_a_youtube_channel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -p "Enter the youtube channel URL (the one which ends with videos): " URL
-yt-dlp -o "%(title)s.%(ext)s" -ciw -v --all-subs --embed-subs --write-sub --merge-output-format mp4 "$URL"
+yt-dlp -o "%(title)s.%(ext)s" -ciw -v --all-subs --embed-subs --write-auto-sub --write-sub --merge-output-format mp4 "$URL"
