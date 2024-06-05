c0b3ccb0402a8d8f6d1d123dc5bd5b948ba6ea98
Jakub Halik <jakubhalik1@gmail.com>
2024-06-05 14:03:33 +0000

    fix

--- iptables_execute_before_deployment_of_http_and_https.sh
+++ iptables_execute_before_deployment_of_http_and_https.sh
@@ -5,6 +5,7 @@ sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 8081 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 8082 -j ACCEPT
-sudo iptables-save > /etc/iptables/iptables.rules
+sudo iptables-save | sudo tee /etc/iptables/iptables.rules > /dev/null
 sudo systemctl enable iptables
 sudo systemctl start iptables
+
