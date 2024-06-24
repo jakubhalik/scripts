57679eaa4b65d232971ecc88fa2fdd05f2ce3b39
Jakub Halik <jakubhalik1@gmail.com>
2024-06-24 19:31:12 +0200

    monitoring

--- monitoring_server.sh
+++ monitoring_server.sh
@@ -5,6 +5,11 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
+if ! command -v pv &> /dev/null; then
+    echo "pv could not be found, installing..."
+    sudo pacman -Syu --noconfirm pv
+fi
+
 mkdir -p /home/x/monitoring
 cd /home/x/monitoring
 
@@ -32,16 +37,30 @@ for i in {0..10}; do
     commands_full[i]="${commands[$i]} | tee monitoring$(($i + 1)).log"
 done
 
+total_commands=${#commands_full[@]}
+current_command=0
+
+update_progress() {
+    current_command=$((current_command + 1))
+    echo -ne "Progress: [$current_command/$total_commands] \r" | pv -pt -e -s $total_commands > /dev/null
+}
+
+trap 'echo -e "\nScript interrupted. Cleaning up..."; kill $(jobs -p); exit' SIGINT
+
 for i in {0..10}; do
     if [[ $i -lt 5 || $i -eq 6 || $i -eq 7 ]]; then
         timeout 60 bash -c "${commands_full[$i]}"
     else
         bash -c "${commands_full[$i]}"
     fi
+    wait $!
+    update_progress
 done
 
 for i in {1..11}; do
     echo -e "\n\n--- Output of monitoring${i}.log ---" >> monitoring.log
     tail -n 70 monitoring${i}.log >> monitoring.log
 done
+
+echo -ne "Progress: [$current_command/$total_commands] \r" | pv -pt -e -s $total_commands > /dev/null
 
