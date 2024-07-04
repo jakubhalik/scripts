1f096e6be5f72b7249c38135da16b67ff05976b7
Jakub Halik <jakubhalik1@gmail.com>
2024-07-04 11:23:04 +0200

    save dwm layout xorg restart and restore dwm layout

--- restart_xorg.sh
+++ restart_xorg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Save the current DWM session
-/home/x/.dwm_session/save_layout.sh
+/home/x/.dwm_session/save_dwm_layout.sh
 
 # Restart Xorg (this might log you out depending on your setup)
 sudo systemctl restart display-manager.service
 
 # Restore the DWM session after restart (you might need to add a delay)
-/home/x/.dwm_session/restore_layout.sh
+/home/x/.dwm_session/restore_dwm_layout.sh
