d13073b793bf3a4241333fe2a46a69b9b5c08d70
Jakub Halik <jakubhalik1@gmail.com>
2024-07-04 11:32:12 +0200

    save dwm layout xorg restart and restore dwm layout

--- restart_xorg.sh
+++ restart_xorg.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Save the current DWM session
 /home/x/.dwm_session/save_dwm_layout.sh
 
-# Kill the current X session
-pkill -KILL -x Xorg
+pkill xinit
 
-# Wait for a moment to ensure Xorg is killed
 sleep 5
 
-# Start a new X session
 startx &
 
-# Restore the DWM session after restart (you might need to add a delay)
-sleep 10  # Adjust the sleep time as needed# Restore the DWM session after restart (you might need to add a delay)
+sleep 10
 /home/x/.dwm_session/restore_dwm_layout.sh
