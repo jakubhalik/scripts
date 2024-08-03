46bc0be8e2512f035cd9820ff2a20e51f1e109d3
Jakub Halik <jakubhalik1@gmail.com>
2024-08-03 12:49:00 +0200

    auto updates 24 h

---
+++ update_pacman_and_aur_every_day.sh
@@ -0,0 +1,21 @@
+#!/bin/dash
+
+get_password() {
+    echo "Enter your password: "
+    stty -echo
+    read password
+    stty echo
+    echo ""
+}
+
+update_system() {
+    echo "$password" | sudo -S pacman -Syu --noconfirm
+    echo "$password" | sudo -S yay -Syu --noconfirm
+}
+
+get_password
+
+while true; do
+    update_system
+    sleep 86400
+done
