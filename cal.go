6c4a70889e7a9a4330ab914b8d7946143229019f
Jakub Halik <jakubhalik1@gmail.com>
2024-08-03 11:05:20 +0200

    my own cli cal, because my week starts from monday, not sunday bruh

---
+++ cal.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+var days_of_week = [...]string{"Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"}
+
+func get_days_of_week() [7]string {
+	return days_of_week
+}
+
+func main() {
+    now := time.Now()
+
+    year, month := now.Year(), now.Month()
+
+	// day, hour, minute, second, nanosecond
+	first_of_month := time.Date(year, month, 1, 0, 0, 0, 0, time.Local)
+
+	// years, months, days
+	last_of_month := first_of_month.AddDate(0, 1, -1)
+
+	fmt.Printf("     %s %d\n", month, year)
+
+	for _, day := range get_days_of_week() {
+		fmt.Printf("%s ", day)
+	}
+	fmt.Println()
+
+	start_day_offset := (int(first_of_month.Weekday()) + 6) % 7
+
+	for i := 0; i < start_day_offset; i++ {
+		fmt.Print("   ")
+	}
+
+	for day := 1; day <= last_of_month.Day(); day++ {
+		fmt.Printf("%2d ", day)
+
+		if (start_day_offset + day) % 7 == 0 {
+			fmt.Println()
+		}
+	}
+
+	if (start_day_offset + last_of_month.Day()) % 7 != 0 {
+		fmt.Println()
+	}
+}
