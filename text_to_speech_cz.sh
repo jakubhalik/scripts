319c1048c42aa67f309025586d7a3e2ff516d475
Jakub Halik <oranzovydzusaja@proton.me>
2024-09-03 17:31:31 +0200

    actually functional text to speech

--- text_to_speech_cz.sh
+++ text_to_speech_cz.sh
@@ -3,17 +3,8 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <input_text_file> <output_audio_file>"
     exit 1
 fi
-
 input_file=$1
 output_file=$2
-
-if [ ! -f "$input_file" ]; then
-    echo "Error: Input file '$input_file' does not exist."
-    exit 1
-fi
-
-text=$(cat "$input_file")
-echo "$text" | text2wave -eval "(voice_czech_ph)" -o "$output_file"
-
+espeak-ng -v cs+f3 -s 100 -f "$input_file" -w "$output_file"
 echo "Conversion complete. Audio saved to $output_file"
 
